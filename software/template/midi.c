33b355a6307cc82828c7cb48903aeaf193334534
Sebastian Neuser <haggl@sineband.de>
2014-02-16 20:54:35 +0100

    Respect 2X setting in USART configuration

--- software/template/midi.c
+++ software/template/midi.c
@@ -49,6 +49,11 @@ void configureUSART( void )
     // apply UBRR value computed by setbaud.h
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
+#if USE_2X
+    UCSR0A |= (1 << U2X0);
+#else
+    UCSR0A &= ~(1 << U2X0);
+#endif
 }
 
 
