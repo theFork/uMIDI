ee3bd1bee1c52c9115eaf7c562016182def59ef4
Sebastian Neuser <haggl@sineband.de>
2015-10-17 20:33:31 +0200

    Add Doxyfile and document LEDs module

--- software/src/lib/leds.c
+++ software/src/lib/leds.c
@@ -1,3 +1,6 @@
+/// \file
+/// \brief  Handling of the on-board LEDs
+
 /*
  * Copyright 2015 Sebastian Neuser
  *
@@ -17,10 +20,6 @@
  * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * LED GPIO port configuration and procedures
-*/
-
 #include "leds.h"
 
 #include <avr/interrupt.h>
@@ -30,6 +29,7 @@
 //                     V A R I A B L E S                      //
 ////////////////////////////////////////////////////////////////
 
+/// \brief  State variable for the green LED
 static struct led_state green_led =
 {
     .active     = false,
@@ -38,6 +38,7 @@ static struct led_state green_led =
     .counter    = 0
 };
 
+/// \brief  State variable for the red LED
 static struct led_state red_led =
 {
     .active     = false,
@@ -46,6 +47,8 @@ static struct led_state red_led =
     .counter    = 0
 };
 
+/// \brief  This array holds pointers to all LED state variables
+/// \see    initialize_leds_module
 static struct led_state *leds[LED_COUNT];
 
 
@@ -53,6 +56,44 @@ static struct led_state *leds[LED_COUNT];
 ////////////////////////////////////////////////////////////////
 //      F U N C T I O N S   A N D   P R O C E D U R E S       //
 ////////////////////////////////////////////////////////////////
+
+/// \brief          Enables or disables an LED by writing the corresponding GPIO output register
+/// \param      pin
+///                 the LED to enable / disable
+/// \param      value
+///                 `true` enables the LED; `false` disables it
+static inline void apply_led(enum led pin, bool value)
+{
+    // Update state varable
+    leds[pin]->active = value;
+
+    // Write GPIO output register
+    if (value) {
+        LED_PORT.OUTSET = _BV(pin);
+    }
+    else {
+        LED_PORT.OUTCLR = _BV(pin);
+    }
+}
+
+/// \brief      Initializes or updates an LED's blinking mode
+/// \details    Aborts if neither mode nor prescaler have changed
+/// \param      led
+///                 state variable of the led
+/// \param      prescaler
+///                 Prescaler for the blinking frequency
+static void set_or_update_blinking_led_state(struct led_state *led, uint8_t prescaler)
+{
+    // Abort if neither mode nor prescaler have changed
+    if (led->mode == LED_BLINK && led->prescaler == prescaler) {
+        return;
+    }
+
+    // Update mode and prescaler and restart counter
+    led->mode = LED_BLINK;
+    led->prescaler = prescaler;
+    led->counter = 0;
+}
 
 void initialize_leds_module(void)
 {
@@ -64,31 +105,9 @@ void initialize_leds_module(void)
     leds[GREEN_LED_BIT] = &green_led;
 }
 
-static inline void apply_led(enum led pin, bool value)
-{
-    leds[pin]->active = value;
-    if (value) {
-        LED_PORT.OUTSET = _BV(pin);
-    }
-    else {
-        LED_PORT.OUTCLR = _BV(pin);
-    }
-}
-
-static void set_or_update_blinking_led_state(struct led_state *led, uint8_t prescaler)
-{
-    // Abort if neither mode or prescaler have changed
-    if (led->mode == LED_BLINK && led->prescaler == prescaler) {
-        return;
-    }
-
-    led->mode = LED_BLINK;
-    led->prescaler = prescaler;
-    led->counter = 0;
-}
-
 void blink_led(enum led led, uint8_t prescaler)
 {
+    // Set blink mode and update prescaler
     if (led == LED_ALL) {
         set_or_update_blinking_led_state(&red_led, prescaler);
         set_or_update_blinking_led_state(&green_led, prescaler);
@@ -100,6 +119,7 @@ void blink_led(enum led led, uint8_t prescaler)
 
 void flash_led(enum led led)
 {
+    // Set flash mode
     if (led == LED_ALL) {
         red_led.mode = LED_FLASH;
         green_led.mode = LED_FLASH;
@@ -111,6 +131,7 @@ void flash_led(enum led led)
 
 void set_led(enum led led, bool value)
 {
+    // Set static mode and enable / disable LED(s)
     if (led == LED_ALL) {
         green_led.mode = LED_STATIC;
         apply_led(LED_GREEN, value);
@@ -125,6 +146,7 @@ void set_led(enum led led, bool value)
 
 void toggle_led(enum led led)
 {
+    // Set static mode and toggle LED(s)
     if (led == LED_ALL) {
         green_led.mode = LED_STATIC;
         apply_led(LED_GREEN, !green_led.active);
@@ -144,20 +166,27 @@ void update_leds(void)
     for (i=0; i<LED_COUNT; i++) {
         switch (leds[i]->mode) {
         case LED_FLASH:
-            if (leds[i]->active) {
+            // Enable the LED in the first call
+            if (!leds[i]->active) {
+                apply_led(i, true);
+            }
+            // Disable the LED in the second call
+            else {
                 leds[i]->mode = LED_STATIC;
                 apply_led(i, false);
-            }
-            else {
-                apply_led(i, true);
             }
             break;
 
         case LED_BLINK:
+            // Increment counter
             ++leds[i]->counter;
+
+            // When the counter reaches the prescaler value
             if (leds[i]->counter >= leds[i]->prescaler) {
+                // Reset the counter
                 leds[i]->counter = 0;
 
+                // Toggle LED
                 apply_led(i, !leds[i]->active);
             }
             break;
--- software/src/lib/leds.h
+++ software/src/lib/leds.h
@@ -1,3 +1,6 @@
+/// \file
+/// \brief  Handling of the on-board LEDs
+
 /*
  * Copyright 2015 Sebastian Neuser
  *
@@ -17,10 +20,6 @@
  * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Header for handling of LED GPIO ports
-*/
-
 #ifndef _LEDS_H
 #define _LEDS_H
 
@@ -31,48 +30,94 @@
 
 
 //---------------- constants ----------------//
+/// \brief  Number of LEDs
 #define     LED_COUNT               2
 
 
 //---------------- AVR PORT mapping ----------------//
+/// \brief  The AVR port the LEDs are connected to
 #define     LED_PORT                PORTE
 
 
 //---------------- AVR bit numbers ----------------//
+/// \brief  The bit number in GPIO registers controlling the red LED
 #define     RED_LED_BIT             0
+
+/// \brief  The bit number in GPIO registers controlling the green LED
 #define     GREEN_LED_BIT           1
 
 
 //---------------- data types ----------------//
+/// \brief      Enumeration for the on-board LEDs
+/// \details    This enumeration is used to select the LED(s) to switch on/off in the service procedures.
 enum led
 {
-    LED_ALL     = -1,
-    LED_GREEN   = GREEN_LED_BIT,
-    LED_RED     = RED_LED_BIT,
+    LED_ALL     = -1,               ///< Select all LEDs
+    LED_GREEN   = GREEN_LED_BIT,    ///< Select the green LED
+    LED_RED     = RED_LED_BIT,      ///< Select the red LED
 };
 
+/// \brief      Enumeration for the LED mode
+/// \details    Depending on the mode, an LED can be turned on/off automatically or manually.
 enum led_mode
 {
-    LED_STATIC,
-    LED_FLASH,
-    LED_BLINK
+    LED_STATIC,                     ///< The LED can be turned on/off manually
+    LED_FLASH,                      ///< The LED flashes briefly and remains off
+    LED_BLINK                       ///< The LED blinks once per second
 };
 
+/// \brief      Internal state of an LED
+/// \details    Used by the service procedures
+/// \see        led_mode
 struct led_state
 {
-    bool            active;
-    enum led_mode   mode;
-    uint8_t         prescaler;
-    uint8_t         counter;
+    bool            active;         ///< When the LED is on, this reads `true`
+    enum led_mode   mode;           ///< The mode of the LED
+    uint8_t         prescaler;      ///< Prescaler for the counter in blinking mode
+    uint8_t         counter;        ///< Counter for the blinking interval
 };
 
 
 //---------------- functions and procedures ----------------//
+/// \brief      Module initialization procedure
+/// \details    Writes relevant AVR GPIO configuration
 void initialize_leds_module(void);
-void blink_led(enum led, uint8_t);
+
+/// \brief      Lets an LED blink in a fixed interval
+/// \details    The prescaler parameter controls the blinking frequency: Every \p prescaler th time
+///             the {@link update_leds} procedure is called, the LED is toggled.
+/// \param      led
+///                 the LED that should blink
+/// \param      prescaler
+///                 controls the blinking frequency
+/// \see        led
+/// \see        update_leds
+void blink_led(enum led, uint8_t prescaler);
+
+/// \brief      Briefly flashes an LED
+/// \param      led
+///                 the LED that should blink
+/// \see        led
 void flash_led(enum led);
-void set_led(enum led, bool);
+
+/// \brief      Enables or disables an LED
+/// \param      led
+///                 the LED that should be enabled/disabled
+/// \param      enable
+///                 `true` switches the LED on; `false` swithes off
+/// \see        led
+void set_led(enum led, bool enable);
+
+/// \brief      Toggles an LED
+/// \param      led
+///                 the LED that should be toggled
+/// \see        led
 void toggle_led(enum led);
+
+/// \brief      State machine task that updates the LEDs
+/// \details    This task must be included in the state machine tasks for the blink and flash modes
+///             to work.
+/// \see        initialize_state_machine
 void update_leds(void);
 
 
