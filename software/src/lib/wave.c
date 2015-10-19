e74621ee9329cb3b653628a906304df285282054
Sebastian Neuser <haggl@sineband.de>
2015-10-19 03:49:39 +0200

    Document function generator module

--- software/src/lib/wave.c
+++ software/src/lib/wave.c
@@ -1,3 +1,6 @@
+/// \file
+/// \brief  Waveform function generator
+
 /*
  * Copyright 2012-2015 Sebastian Neuser
  *
@@ -17,10 +20,6 @@
  * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Wave computations module.
-*/
-
 #include <stdlib.h>
 
 #include "midi.h"
@@ -32,11 +31,9 @@
 //                     V A R I A B L E S                      //
 ////////////////////////////////////////////////////////////////
 
-static struct wave_settings *settings;
-static struct wave_state    *state;
-
+/// \brief      Internal array of amplitudes for the WAVE_PATTERN_nn waveforms
 static uint8_t wave_patterns[16][16] = {
-    { // WAVE_TABLE_01
+    { // WAVE_PATTERN_01
         WHAMMY_AMPLITUDE_UNISON,
         WHAMMY_AMPLITUDE_MINOR_SECOND,
         WHAMMY_AMPLITUDE_MAJOR_SECOND,
@@ -54,7 +51,7 @@ static uint8_t wave_patterns[16][16] = {
         WHAMMY_AMPLITUDE_OCTAVE,
         WHAMMY_AMPLITUDE_OCTAVE,
     },
-    { // WAVE_TABLE_02
+    { // WAVE_PATTERN_02
         WHAMMY_AMPLITUDE_UNISON,
         WHAMMY_AMPLITUDE_MINOR_THIRD,
         WHAMMY_AMPLITUDE_PERFECT_FOURTH,
@@ -72,7 +69,7 @@ static uint8_t wave_patterns[16][16] = {
         WHAMMY_AMPLITUDE_UNISON,
         WHAMMY_AMPLITUDE_MINOR_THIRD,
     },
-    { // WAVE_TABLE_02
+    { // WAVE_PATTERN_03
         WHAMMY_AMPLITUDE_UNISON,
         WHAMMY_AMPLITUDE_1ST_MINOR_THIRD,
         WHAMMY_AMPLITUDE_1ST_PERFECT_FIFTH,
@@ -94,24 +91,29 @@ static uint8_t wave_patterns[16][16] = {
 
 
 
-////////////////////////////////////////////////////////////////
-//      F U N C T I O N S   A N D   P R O C E D U R E S       //
-////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+//  S T A T I C   F U N C T I O N S   A N D   P R O C E D U R E S  //
+/////////////////////////////////////////////////////////////////////
 
-static void advance_step_counter(void)
+/// \brief      Increments or decrements the step counter depending on its the current direction
+/// \details    When the top or bottom value is reached, the direction is switched automatically.
+///             The step counter consequently follows a triangle wave.
+/// \param      wave
+///                 the wave
+static void advance_step_counter(struct wave* wave)
 {
-    switch (state->step_direction) {
+    switch (wave->state.step_direction) {
         case DIRECTION_DOWN:
             // Decrement counter; switch direction at min value
-            if (--state->step_counter <= 0) {
-                state->step_direction = DIRECTION_UP;
+            if (--wave->state.step_counter <= 0) {
+                wave->state.step_direction = DIRECTION_UP;
             }
             break;
 
         case DIRECTION_UP:
             // Increment counter; switch direction at max value
-            if (++state->step_counter >= MIDI_MAX_VALUE) {
-                state->step_direction = DIRECTION_DOWN;
+            if (++wave->state.step_counter >= MIDI_MAX_VALUE) {
+                wave->state.step_direction = DIRECTION_DOWN;
             }
             break;
 
@@ -120,32 +122,41 @@ static void advance_step_counter(void)
     }
 }
 
-static uint8_t compute_ramp(void)
+/// \brief      Derives a rising ramp from the step counter
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_ramp(struct wave* wave)
 {
-    switch (state->step_direction) {
+    switch (wave->state.step_direction) {
         case DIRECTION_DOWN:
-            return MIDI_MAX_VALUE - state->step_counter / 2;
+            return MIDI_MAX_VALUE - wave->state.step_counter / 2;
 
         case DIRECTION_UP:
-            return state->step_counter / 2;
+            return wave->state.step_counter / 2;
 
         default:
             return 0;
     }
 }
 
-static uint8_t compute_random_wave(void)
+/// \brief      Computes a random wave
+/// \details    Note that this uses static variables, so only one random wave can be computed!
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_random_wave(struct wave* wave)
 {
     static uint8_t current_value = 0;
 
     // Check if step counter has changed
     static uint8_t old_step_counter = 0;
-    if (state->step_counter == old_step_counter) {
+    if (wave->state.step_counter == old_step_counter) {
         return current_value;
     }
-    old_step_counter = state->step_counter;
+    old_step_counter = wave->state.step_counter;
 
-    // Adjust output randomly
+    // Adjust output randomly but not beyond MIDI value range
     if (rand() % 2 && current_value <= MIDI_MAX_VALUE - RANDOM_WAVE_STEP_SIZE) {
         current_value += RANDOM_WAVE_STEP_SIZE;
     }
@@ -155,67 +166,105 @@ static uint8_t compute_random_wave(void)
     return current_value;
 }
 
-static uint8_t compute_saw_down_wave(void)
+/// \brief      Computes a saw down wave
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_saw_down_wave(struct wave* wave)
 {
-    return MIDI_MAX_VALUE - compute_ramp();
+    return MIDI_MAX_VALUE - compute_ramp(wave);
 }
 
-static uint8_t compute_saw_up_wave(void)
+/// \brief      Computes a saw up wave
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_saw_up_wave(struct wave* wave)
 {
-    return compute_ramp();
+    return compute_ramp(wave);
 }
 
-static uint8_t compute_sine_wave(void)
+/// \brief      Computes a sine wave through a lookup table
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_sine_wave(struct wave* wave)
 {
-    switch (state->step_direction) {
+    switch (wave->state.step_direction) {
         case DIRECTION_DOWN:
-            return MIDI_MAX_VALUE - lookup_sine(state->step_counter);
+            return MIDI_MAX_VALUE - lookup_sine(wave->state.step_counter);
 
         case DIRECTION_UP:
-            return lookup_sine(state->step_counter);
+            return lookup_sine(wave->state.step_counter);
 
         default:
             return 0;
     }
 }
 
-static uint8_t compute_square_wave(void)
+/// \brief      Computes a square wave
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_square_wave(struct wave* wave)
 {
-    return (state->step_direction == DIRECTION_UP) ? MIDI_MAX_VALUE : 0;
+    return (wave->state.step_direction == DIRECTION_UP) ? MIDI_MAX_VALUE : 0;
 }
 
-static uint8_t compute_stairs_wave(void)
+/// \brief      Computes a square wave
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_stairs_wave(struct wave* wave)
 {
     uint8_t step_size = MIDI_MAX_VALUE / STAIR_WAVE_STEPS;
 
     // Reinitialize step counter at edge values
-    uint8_t *counter = &(state->step_counter);
+    uint8_t *counter = &(wave->state.step_counter);
     if (*counter == 0) {
         *counter = step_size;
     }
     else if (*counter > step_size * STAIR_WAVE_STEPS) {
         *counter = step_size * STAIR_WAVE_STEPS - step_size;
-        state->step_direction = DIRECTION_DOWN;
+        wave->state.step_direction = DIRECTION_DOWN;
     }
 
     // Return quantized counter
     return (*counter / step_size) * step_size;
 }
 
-static uint8_t compute_triangle_wave(void)
+/// \brief      Computes a triangle wave
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_triangle_wave(struct wave* wave)
 {
-    return state->step_counter;
+    return wave->state.step_counter;
 }
 
-static uint8_t compute_wave_pattern(uint8_t pattern_number)
+/// \brief      Computes a wave according to the specified pattern
+/// \param      wave
+///                 the wave
+/// \return     the wave output
+static uint8_t compute_wave_pattern(struct wave* wave)
 {
+    // Compute sample coordinates
+    uint8_t pattern_number = wave->settings.waveform - WAVE_PATTERN_01;
     uint8_t quantization = MIDI_MAX_VALUE / 16;
-    uint8_t sample_index = compute_ramp() / quantization;
+    uint8_t sample_index = compute_ramp(wave) / quantization;
+
+    // Reset step counter after last sample
     if (sample_index >= 16) {
-        state->step_counter = 0;
+        wave->state.step_counter = 0;
     }
     return wave_patterns[pattern_number][sample_index];
 }
+
+
+
+////////////////////////////////////////////////////////////////
+//      F U N C T I O N S   A N D   P R O C E D U R E S       //
+////////////////////////////////////////////////////////////////
 
 void initialize_wave(struct wave* wave, enum waveform waveform, uint8_t speed, uint8_t amplitude, uint8_t offset)
 {
@@ -243,60 +292,56 @@ void set_waveform(struct wave *wave, enum waveform waveform)
 
 uint8_t update_wave(struct wave *wave)
 {
-    // Define shortcuts
-    settings = &(wave->settings);
-    state = &(wave->state);
-
     // Increment speed counter and reset it if the prescaler was reached
-    ++state->speed_counter;
-    if (state->speed_counter >= state->speed_prescaler) {
-        state->speed_counter = 0;
+    ++wave->state.speed_counter;
+    if (wave->state.speed_counter >= wave->state.speed_prescaler) {
+        wave->state.speed_counter = 0;
 
         // Advance step counter
-        advance_step_counter();
+        advance_step_counter(wave);
     }
 
     // Compute and return wave value
     uint16_t output;
-    switch (settings->waveform) {
+    switch (wave->settings.waveform) {
         case WAVE_OFF:
             return 0;
 
         case WAVE_RANDOM:
-            output = compute_random_wave();
+            output = compute_random_wave(wave);
             break;
 
         case WAVE_SAW_DOWN:
-            output = compute_saw_down_wave();
+            output = compute_saw_down_wave(wave);
             break;
 
         case WAVE_SAW_UP:
-            output = compute_saw_up_wave();
+            output = compute_saw_up_wave(wave);
             break;
 
         case WAVE_SINE:
-            output = compute_sine_wave();
+            output = compute_sine_wave(wave);
             break;
 
         case WAVE_SQUARE:
-            output = compute_square_wave();
+            output = compute_square_wave(wave);
             break;
 
         case WAVE_STAIRS:
-            output = compute_stairs_wave();
+            output = compute_stairs_wave(wave);
             break;
 
         case WAVE_TRIANGLE:
-            output = compute_triangle_wave();
+            output = compute_triangle_wave(wave);
             break;
 
         default:
-            output = compute_wave_pattern(settings->waveform - WAVE_PATTERN_01);
+            output = compute_wave_pattern(wave);
     }
 
     // Amplify and add offset
-    output *= settings->amplitude;
+    output *= wave->settings.amplitude;
     output /= MIDI_MAX_VALUE;
-    output += settings->offset;
+    output += wave->settings.offset;
     return output;
 }
--- software/src/lib/wave.h
+++ software/src/lib/wave.h
@@ -1,3 +1,6 @@
+/// \file
+/// \brief  Waveform function generator
+
 /*
  * Copyright 2012-2015 Sebastian Neuser
  *
@@ -17,10 +20,6 @@
  * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Header for wave computations.
-*/
-
 #ifndef _WAVE_H
 #define _WAVE_H
 
@@ -30,108 +29,156 @@
 
 
 //---------------- code macros ----------------//
-// step size of the random wave
+
+/// \brief      Step size of the random wave
+/// \details    The output will change in a random direction by this amount
 #define     RANDOM_WAVE_STEP_SIZE           4
 
-// the number of levels in the stair wave
+/// \brief       Number of levels in the stair wave
 #define     STAIR_WAVE_STEPS                5
 
 
 //---------------- data types ----------------//
+
+/// \brief      Step counter direction
 enum direction
 {
-    DIRECTION_DOWN,
-    DIRECTION_UP,
+    DIRECTION_DOWN,                             ///< The counter is counting down
+    DIRECTION_UP,                               ///< The counter is counting up
 };
 
+/// \brief      Amplitudes that represent certain intervals on a DigiTech Whammy pedal
 enum whammy_amplitude
 {
-    WHAMMY_AMPLITUDE_UNISON             = 0,
-    WHAMMY_AMPLITUDE_MINOR_SECOND       = 13,
-    WHAMMY_AMPLITUDE_MAJOR_SECOND       = 24,
-    WHAMMY_AMPLITUDE_MINOR_THIRD        = 32,
-    WHAMMY_AMPLITUDE_MAJOR_THIRD        = 42,
-    WHAMMY_AMPLITUDE_PERFECT_FOURTH     = 52,
-    WHAMMY_AMPLITUDE_TRITONE            = 63,
-    WHAMMY_AMPLITUDE_PERFECT_FIFTH      = 74,
-    WHAMMY_AMPLITUDE_MINOR_SIXTH        = 84,
-    WHAMMY_AMPLITUDE_MAJOR_SIXTH        = 95,
-    WHAMMY_AMPLITUDE_MINOR_SEVENTH      = 106,
-    WHAMMY_AMPLITUDE_MAJOR_SEVENTH      = 116,
-    WHAMMY_AMPLITUDE_OCTAVE             = 127,
-    WHAMMY_AMPLITUDE_1ST_MINOR_SECOND   = 5,
-    WHAMMY_AMPLITUDE_1ST_MAJOR_SECOND   = 10,
-    WHAMMY_AMPLITUDE_1ST_MINOR_THIRD    = 16,
-    WHAMMY_AMPLITUDE_1ST_MAJOR_THIRD    = 21,
-    WHAMMY_AMPLITUDE_1ST_PERFECT_FOURTH = 26,
-    WHAMMY_AMPLITUDE_1ST_TRITONE        = 32,
-    WHAMMY_AMPLITUDE_1ST_PERFECT_FIFTH  = 37,
-    WHAMMY_AMPLITUDE_1ST_MINOR_SIXTH    = 42,
-    WHAMMY_AMPLITUDE_1ST_MAJOR_SIXTH    = 48,
-    WHAMMY_AMPLITUDE_1ST_MINOR_SEVENTH  = 53,
-    WHAMMY_AMPLITUDE_1ST_MAJOR_SEVENTH  = 58,
-    WHAMMY_AMPLITUDE_1ST_OCTAVE         = 63,
-    WHAMMY_AMPLITUDE_2ND_MINOR_SECOND   = 69,
-    WHAMMY_AMPLITUDE_2ND_MAJOR_SECOND   = 74,
-    WHAMMY_AMPLITUDE_2ND_MINOR_THIRD    = 79,
-    WHAMMY_AMPLITUDE_2ND_MAJOR_THIRD    = 85,
-    WHAMMY_AMPLITUDE_2ND_PERFECT_FOURTH = 90,
-    WHAMMY_AMPLITUDE_2ND_TRITONE        = 95,
-    WHAMMY_AMPLITUDE_2ND_PERFECT_FIFTH  = 100,
-    WHAMMY_AMPLITUDE_2ND_MINOR_SIXTH    = 106,
-    WHAMMY_AMPLITUDE_2ND_MAJOR_SIXTH    = 111,
-    WHAMMY_AMPLITUDE_2ND_MINOR_SEVENTH  = 117,
-    WHAMMY_AMPLITUDE_2ND_MAJOR_SEVENTH  = 122,
-    WHAMMY_AMPLITUDE_2ND_OCTAVE         = 127,
+    WHAMMY_AMPLITUDE_UNISON             = 0,    ///< Unison, valid in every mode
+    WHAMMY_AMPLITUDE_MINOR_SECOND       = 13,   ///< Minor second in 1oct mode
+    WHAMMY_AMPLITUDE_MAJOR_SECOND       = 24,   ///< Major second in 1oct mode
+    WHAMMY_AMPLITUDE_MINOR_THIRD        = 32,   ///< Minor third in 1oct mode
+    WHAMMY_AMPLITUDE_MAJOR_THIRD        = 42,   ///< Major third in 1oct mode
+    WHAMMY_AMPLITUDE_PERFECT_FOURTH     = 52,   ///< Perfect fourth in 1oct mode
+    WHAMMY_AMPLITUDE_TRITONE            = 63,   ///< Tritone in 1oct mode
+    WHAMMY_AMPLITUDE_PERFECT_FIFTH      = 74,   ///< Perfect fifth in 1oct mode
+    WHAMMY_AMPLITUDE_MINOR_SIXTH        = 84,   ///< Minor sixth in 1oct mode
+    WHAMMY_AMPLITUDE_MAJOR_SIXTH        = 95,   ///< Major sixth in 1oct mode
+    WHAMMY_AMPLITUDE_MINOR_SEVENTH      = 106,  ///< Minor seventh in 1oct mode
+    WHAMMY_AMPLITUDE_MAJOR_SEVENTH      = 116,  ///< Major seventh in 1oct mode
+    WHAMMY_AMPLITUDE_OCTAVE             = 127,  ///< Octave in 1oct mode
+    WHAMMY_AMPLITUDE_1ST_MINOR_SECOND   = 5,    ///< First minor second in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_MAJOR_SECOND   = 10,   ///< First major second in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_MINOR_THIRD    = 16,   ///< First minor third in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_MAJOR_THIRD    = 21,   ///< First major third in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_PERFECT_FOURTH = 26,   ///< First perfect fourth in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_TRITONE        = 32,   ///< First tritone in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_PERFECT_FIFTH  = 37,   ///< First perfect fifth in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_MINOR_SIXTH    = 42,   ///< First minor sixth in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_MAJOR_SIXTH    = 48,   ///< First major sixth in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_MINOR_SEVENTH  = 53,   ///< First minor seventh in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_MAJOR_SEVENTH  = 58,   ///< First major seventh in 2oct mode
+    WHAMMY_AMPLITUDE_1ST_OCTAVE         = 63,   ///< First octave in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MINOR_SECOND   = 69,   ///< Second minor second in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MAJOR_SECOND   = 74,   ///< Second major second in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MINOR_THIRD    = 79,   ///< Second minor third in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MAJOR_THIRD    = 85,   ///< Second major third in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_PERFECT_FOURTH = 90,   ///< Second perfect fourth in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_TRITONE        = 95,   ///< Second tritone in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_PERFECT_FIFTH  = 100,  ///< Second perfect fifth in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MINOR_SIXTH    = 106,  ///< Second minor sixth in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MAJOR_SIXTH    = 111,  ///< Second major sixth in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MINOR_SEVENTH  = 117,  ///< Second minor seventh in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_MAJOR_SEVENTH  = 122,  ///< Second major seventh in 2oct mode
+    WHAMMY_AMPLITUDE_2ND_OCTAVE         = 127,  ///< Second octave in 2oct mode
 };
 
+/// \brief      Enumeration of available waveforms
 enum waveform
 {
-    WAVE_OFF,
-    WAVE_SINE,
-    WAVE_TRIANGLE,
-    WAVE_SAW_UP,
-    WAVE_SAW_DOWN,
-    WAVE_SQUARE,
-    WAVE_STAIRS,
-    WAVE_RANDOM,
-    WAVE_PATTERN_01,
-    WAVE_PATTERN_02,
-    WAVE_PATTERN_03,
-    WAVE_PATTERN_04,
-    WAVE_PATTERN_05,
-    WAVE_PATTERN_06,
-    WAVE_PATTERN_07,
-    WAVE_PATTERN_08,
+    WAVE_OFF,                                   ///< No waveform
+    WAVE_SINE,                                  ///< Sine wave
+    WAVE_TRIANGLE,                              ///< Triangle wave
+    WAVE_SAW_UP,                                ///< Saw up wave
+    WAVE_SAW_DOWN,                              ///< Saw down wave
+    WAVE_SQUARE,                                ///< Square wave
+    WAVE_STAIRS,                                ///< Stair wave
+    WAVE_RANDOM,                                ///< Random wave
+    WAVE_PATTERN_01,                            ///< Wave pattern 01
+    WAVE_PATTERN_02,                            ///< Wave pattern 02
+    WAVE_PATTERN_03,                            ///< Wave pattern 03
+    WAVE_PATTERN_04,                            ///< Wave pattern 04
+    WAVE_PATTERN_05,                            ///< Wave pattern 05
+    WAVE_PATTERN_06,                            ///< Wave pattern 06
+    WAVE_PATTERN_07,                            ///< Wave pattern 07
+    WAVE_PATTERN_08,                            ///< Wave pattern 08
 };
 
+/// \brief      Wave configuration
+/// \see        waveform
 struct wave_settings
 {
-    enum waveform   waveform;
-    uint8_t         speed;
-    uint8_t         amplitude;
-    uint8_t         offset;
+    enum waveform   waveform;                   ///< Waveform
+    uint8_t         speed;                      ///< Speed of the wave [0, 127]
+    uint8_t         amplitude;                  ///< Amplitude [0, 127]
+    uint8_t         offset;                     ///< Static offset [0, 127]
 };
 
+/// \brief      Internal state of a wave
+/// \see        direction
 struct wave_state
 {
-    uint8_t         speed_prescaler;
-    uint8_t         speed_counter;
-    uint8_t         step_counter;
-    enum direction  step_direction;
+    uint8_t         speed_prescaler;            ///< Prescaler value for the speed counter
+    uint8_t         speed_counter;              ///< Current speed counter value
+    uint8_t         step_counter;               ///< Current step counter value
+    enum direction  step_direction;             ///< Current step counter direction
 };
 
+/// \brief      The settings and internal state of a wave
+/// \see        wave_settings
+/// \see        wave_state
 struct wave
 {
-    struct wave_settings    settings;
-    struct wave_state       state;
+    struct wave_settings    settings;           ///< Settings
+    struct wave_state       state;              ///< Internal state
 };
 
 
 //---------------- functions and procedures ----------------//
-void initialize_wave(struct wave* wave, enum waveform waveform, uint8_t speed, uint8_t amplitude, uint8_t offset);
-void set_speed(struct wave*, uint8_t);
+
+/// \brief      Initializes a wave
+/// \param      wave
+///                 the wave to initialize
+/// \param      waveform
+///                 the initial waveform
+/// \param      speed
+///                 the initial speed
+/// \param      amplitude
+///                 the amplitude
+/// \param      offset
+///                 the static offset
+/// \see        wave
+/// \see        waveform
+void initialize_wave(struct wave* wave, enum waveform waveform, uint8_t speed,
+                     uint8_t amplitude, uint8_t offset);
+
+/// \brief      Updates the speed of a wave
+/// \param      wave
+///                 the wave to update
+/// \param      speed
+///                 the new speed
+/// \see        wave
+void set_speed(struct wave*, uint8_t speed);
+
+/// \brief      Updates the waveform of a wave
+/// \param      wave
+///                 the wave to update
+/// \param      waveform
+///                 the new waveform
 void set_waveform(struct wave*, enum waveform);
+
+/// \brief      Computes the current wave output value
+/// \details    This function must be called cyclically. Over time the output value follows the
+///             configured waveform.
+/// \param      wave
+///                 the wave whose output should be computed
+/// \return     the current output value
 uint8_t update_wave(struct wave*);
 
 
