a86d288887e78268cf6f5c2fdedf3464fca62b0f
simon gansen <simon@sineband.de>
2015-12-19 21:14:25 +0100

    Add math module with linear function

---
+++ software/src/lib/math.c
@@ -0,0 +1,45 @@
+/// \file
+
+/*
+ * Copyright 2015 Simon Gansen
+ *
+ * This file is part of the uMIDI firmware.
+ *
+ * the uMIDI firmware is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * the uMIDI firmware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//---------------- includes ----------------//
+#include "midi.h"
+#include "math.h"
+
+
+////////////////////////////////////////////////////////////////
+//                     V A R I A B L E S                      //
+////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////
+//      F U N C T I O N S   A N D   P R O C E D U R E S       //
+////////////////////////////////////////////////////////////////
+
+void init_linear(struct linear_config* config)
+{
+    config->slope = (config->max - config->offset) / MIDI_MAX_VALUE;
+}
+
+uint16_t linear(struct linear_config* config, uint8_t midi_value)
+{
+    return config->slope * midi_value + config->offset;
+}
---
+++ software/src/lib/math.h
@@ -0,0 +1,58 @@
+/// \file
+/// \brief  TODO
+/// \details TODO
+
+/*
+ * Copyright 2015 Simon Gansen
+ *
+ * This file is part of the uMIDI firmware.
+ *
+ * the uMIDI firmware is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * the uMIDI firmware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _MATH_H
+#define _MATH_H
+
+//---------------- includes -----------------//
+#include <stdbool.h>
+#include <avr/io.h>
+
+
+//---------------- constants ----------------//
+
+
+//---------------- data types ---------------//
+
+/// \brief      Configuration for a linear function.
+struct linear_config
+{
+    uint16_t max;       ///< The value that will be returned by linear() whenn called with midi_value = 127
+    uint16_t offset;    ///< The value that will be returned by linear() when called with midi_value = 0
+    uint16_t slope;     ///< The slope of the linear function (computed in init_linear())
+};
+
+
+//---------------- linears ----------------//
+
+/// \brief      Initializes a linear function
+/// \param      config
+///                 the configuration of the linear funcion to initialize
+void init_linear(struct linear_config* config);
+
+/// \brief      Calculates a linear function from a given midi value (0 .. 127)
+/// \param      config
+///                 the configuration of the linear funcion to initialize
+uint16_t linear(struct linear_config* config, uint8_t midi_value);
+
+#endif // _MATH_H
