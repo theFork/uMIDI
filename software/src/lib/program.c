5f1cd2bcb962a739ef812123d7aca9be5f24b5e2
Sebastian Neuser <haggl@sineband.de>
2015-12-21 04:22:49 +0100

    Document program module

--- software/src/lib/program.c
+++ software/src/lib/program.c
@@ -1,20 +1,23 @@
+/// \file
+/// \brief      Program storage module implementation
+
 /*
- * Copyright 2012-2015 Sebastian Neuser
+ * Copyright 2015 Sebastian Neuser
  *
- * This file is part of the MIDI switcher firmware.
+ * This file is part of the uMIDI firmware.
  *
- * The MIDI volume controller firmware is free software: you can redistribute it and/or modify
+ * the uMIDI firmware is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The MIDI volume controller firmware is distributed in the hope that it will be useful,
+ * the uMIDI firmware is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with the MIDI switcher firmware.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -33,10 +36,10 @@
 //                     V A R I A B L E S                      //
 ////////////////////////////////////////////////////////////////
 
-// EEPROM program storage
+/// \brief      EEPROM program storage
 static uint16_t program_data_storage[PROGRAM_COUNT] EEMEM;
 
-// Current program
+/// \brief      Current program
 static struct program current_program;
 
 /// \brief      Callback for program execution
@@ -48,17 +51,6 @@ static void (*execute_program)(uint16_t);
 //      F U N C T I O N S   A N D   P R O C E D U R E S       //
 ////////////////////////////////////////////////////////////////
 
-void init_program_module(void (*execute_callback)(uint16_t))
-{
-    execute_program = execute_callback;
-}
-
-void copy_current_program_to(uint8_t target_program)
-{
-    // Copy program
-    update_program_data(target_program, current_program.data);
-}
-
 void copy_current_bank_to(uint8_t target_bank)
 {
     // Compute the source bank
@@ -68,14 +60,14 @@ void copy_current_bank_to(uint8_t target_bank)
     uint16_t data;
     uint8_t program_index;
     for (program_index=0; program_index<10; program_index++) {
-        // bank 0 contains only 9 programs, so we have to skip certain actions
+        // Bank 0 contains only 9 programs, so we have to skip certain actions
         if (program_index == 9) {
-            // if bank 0 is the target, do nothing
+            // If bank 0 is the target, do nothing
             if (target_bank == 0) {
                 break;
             }
 
-            // bank 0 is the source
+            // If bank 0 is the source, write an empty program
             if (sourceBank == 0) {
                 data = 0;
             }
@@ -84,8 +76,14 @@ void copy_current_bank_to(uint8_t target_bank)
             data = read_program_data(sourceBank + program_index);
         }
 
-        update_program_data((target_bank*10) + program_index, data);
+        write_program((target_bank*10) + program_index, data);
     }
+}
+
+void copy_current_program_to(uint8_t target_program)
+{
+    // Copy program
+    write_program(target_program, current_program.data);
 }
 
 void enter_program(uint8_t number)
@@ -95,26 +93,20 @@ void enter_program(uint8_t number)
         return;
     }
 
-    // Load program and set outputs
+    // Load program and execute program
     current_program.number = number;
     current_program.data = read_program_data(number);
-    // TODO apply_program_data(current_program.data);
+    execute_program(current_program.data);
+}
+
+void init_program_module(void (*execute_program_callback)(uint16_t))
+{
+    execute_program = execute_program_callback;
 }
 
 uint16_t read_program_data(uint8_t number)
 {
     return eeprom_read_word(&program_data_storage[number]);
-}
-
-void update_program_data(uint8_t number, uint16_t data)
-{
-    eeprom_write_word(&program_data_storage[number], data);
-}
-
-void wipe_current_program(void)
-{
-    update_program_data(current_program.number, 0);
-    enter_program(current_program.number);
 }
 
 void wipe_current_bank(void)
@@ -123,9 +115,20 @@ void wipe_current_bank(void)
     uint8_t bank = ( (current_program.number+1) / 10 ) * 10;
     uint8_t i;
     for (i=0; i<10; i++) {
-        update_program_data(bank+i, 0);
+        write_program(bank+i, 0);
     }
 
     // Apply the changes
     enter_program(current_program.number);
+}
+
+void wipe_current_program(void)
+{
+    write_program(current_program.number, 0);
+    enter_program(current_program.number);
+}
+
+void write_program(uint8_t number, uint16_t data)
+{
+    eeprom_write_word(&program_data_storage[number], data);
 }
--- software/src/lib/program.h
+++ software/src/lib/program.h
@@ -1,20 +1,43 @@
+/// \file
+/// \brief      Program storage API
+/// \details    This module takes care of loading, storing, copying and wiping program data.
+///             The programs are stored in EEPROM as 16-bit words - providing a generic container
+///             for flags, numbers - whatever is suitable for your program. It is advisable to
+///             define a `union` for easier access of these components in your application code -
+///             for example something like
+///             \code
+///             union program_data = {
+///                 uint16_t word;
+///                 struct {
+///                     bool flag0 : 1;
+///                     bool flag1 : 1;
+///                     bool flag2 : 1;
+///                 } bit;
+///             }
+///             \endcode
+///             The programs are grouped into twelve banks of ten programs each - leaving 128 bits
+///             of EEPROM for other application specific data.
+///             Applications must implement and register a callback for program execution, which
+///             does whatever is necessary when a new program is loaded.
+/// \see        init_program_module
+
 /*
- * Copyright 2012-2015 Sebastian Neuser
+ * Copyright 2015 Sebastian Neuser
  *
- * This file is part of the MIDI switcher firmware.
+ * This file is part of the uMIDI firmware.
  *
- * The MIDI volume controller firmware is free software: you can redistribute it and/or modify
+ * the uMIDI firmware is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The MIDI volume controller firmware is distributed in the hope that it will be useful,
+ * the uMIDI firmware is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with the MIDI switcher firmware.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -30,40 +53,73 @@
 
 
 //---------------- constants ----------------//
+/// \brief      The maximum number of programs that can be stored
 #define     PROGRAM_COUNT               120
 
 
 //---------------- data types ----------------//
 
+/// \brief      Data structure for program information
 struct program
 {
-    uint8_t     number;
-    uint16_t    data;
+    uint8_t     number;     ///< Program number [0..#PROGRAM_COUNT-1].
+    uint16_t    data;       ///< The application specific program data as a 16-bit word.
 };
 
 
 //---------------- functions and procedures ----------------//
 
-/// \brief      Callback for program execution
-/// \details    This function is called whenever a new program is entered after configuration
-///             data was read from EEPROM memory.
-/// \param      program_data
-///                 This is the program configuration data that was read from memory.
-void init_program_module(void (*execute_callback)(uint16_t));
+/// \brief      Copies all programs in the current bank to another bank.
+/// \details    Irreversibly overwrites all programs in the target bank.
+/// \param      target_bank
+///                 Target bank number [0..11].
+void copy_current_bank_to(uint8_t target_bank);
 
-void copy_current_bank_to(uint8_t bank_number);
+/// \brief      Copies the current program to the specified position in memory.
+/// \details    Irreversibly overwrites the target program.
+/// \param      target_program
+///                 Target program number [0..119].
+void copy_current_program_to(uint8_t target_program);
 
-void copy_current_program_to(uint8_t target_bank);
-
+/// \brief      Loads and executes the specified program.
+/// \details    Reads the new program data from EEPROM, updates the internal state and calls the
+///             registered callback function, which executes the program.
+///             If the specified program number is the same as the stored current program number,
+///             this function does nothing.
+/// \param      number
+///                 Program number [0..119].
+/// \see init_program_module
 void enter_program(uint8_t number);
 
+/// \brief      Initializes the program storage module.
+/// \details    Registers a provided callback function, which is executed whenever the current
+///             program data word is updated either by wiping the program or loading a new one.
+/// \param      execute_program_callback
+///                 Callback for program execution
+///                 \param program_data
+///                        The program configuration data that was read from memory.
+void init_program_module(void (*execute_program_callback)(uint16_t program_data));
+
+/// \brief      Reads a program data word from the specified position in memory.
+/// \param      number
+///                 Program number [0..119].
 uint16_t read_program_data(uint8_t number);
 
-void update_program_data(uint8_t number, uint16_t data);
+/// \brief      Reinitializes all programs in the current bank.
+/// \see        wipe_current_program
+void wipe_current_bank(void);
 
+/// \brief      Reinitializes the currently loaded program.
+/// \details    Clears all bits in the program data word, stores it and executes the initialized
+///             program.
 void wipe_current_program(void);
 
-void wipe_current_bank(void);
+/// \brief      Stores a given program data word in the specified position in memory.
+/// \param      number
+///                 Program number [0..119].
+/// \param      data
+///                 Program data as 16-bit word.
+void write_program(uint8_t number, uint16_t data);
 
 
 //---------------- EOF ----------------//
