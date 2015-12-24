27d19b8bc54b35b42c0165b7ddeb035d8018079f
Sebastian Neuser <haggl@sineband.de>
2015-12-25 00:44:15 +0100

    Prepare firmware update command

--- software/src/template-usb/app.c
+++ software/src/template-usb/app.c
@@ -20,6 +20,7 @@
  * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
 #include <string.h>
 
 #include "lib/background_tasks.h"
@@ -48,10 +49,21 @@ static char cmd_buffer[CMD_BUFFER_SIZE] = "";
 /// \brief      Command buffer write index
 static uint8_t cmd_buffer_index = 0;
 
-static bool raw_data_mode = false;
-static uint16_t data_size = 0;
-static char data_buffer[CMD_BUFFER_SIZE] = "";
-static uint8_t data_buffer_index = 0;
+/// \brief      This flag indicates if an update is in progress
+static bool update_in_progress = false;
+
+/// \brief      This number stores the size of an incoming update packet
+static uint16_t update_bytes_pending = 0;
+
+/// \brief      Buffer for application pages
+static uint8_t page_buffer[SPM_PAGESIZE] = "";
+
+/// \brief      Page buffer write index
+static uint16_t page_buffer_index = 0;
+
+/// \brief      Temporary application write address
+static uint32_t temp_app_addr = 0;
+
 
 
 ////////////////////////////////////////////////////////////////
@@ -64,6 +76,8 @@ static inline bool exec_help(void)
     usb_puts("Welcome to the uMIDI serial interface!");
     usb_puts("Here is a list of available commands:");
     usb_puts("    clear        :  Clears the console by printing CR/LFs.");
+    usb_puts("    fwupdate <s> :  Initiates a firmware update:");
+    usb_puts("                    <s>: firmware update packet size");
     usb_puts("    help         :  Prints this help message.");
     usb_puts("    led <l> <a>  :  Manipulates the two on-board LEDs:");
     usb_puts("                    <l> : LED to manipulate");
@@ -131,6 +145,24 @@ static inline bool exec_led(const char* command)
     return true;
 }
 
+static inline bool exec_update(const char* command)
+{
+    // Parse size of incoming update packet
+    update_bytes_pending = atoi(command + 9);
+    if (update_bytes_pending == 0) {
+        return false;
+    }
+    if (update_bytes_pending > SPM_PAGESIZE) {
+        usb_puts("Buffer is too small!\n\r");
+        return false;
+    }
+
+    usb_printf("Ready to receive %u bytes of raw data...\n\r", update_bytes_pending);
+    usb_set_echo(false);
+    update_in_progress = true;
+    return true;
+}
+
 static void execute_command(const char* command)
 {
     bool success = true;
@@ -154,6 +186,10 @@ static void execute_command(const char* command)
         success = exec_led(command);
     }
 
+    else if (strncmp(command, "fwupdate ", 9) == 0) {
+        success = exec_update(command);
+    }
+
     else if (strcmp(command, "reset") == 0) {
         usb_puts("Resetting device...");
         reset = true;
@@ -171,6 +207,41 @@ static void execute_command(const char* command)
     // Clear command buffer and reset write pointer
     memset(cmd_buffer, '\0', sizeof(cmd_buffer));
     cmd_buffer_index = 0;
+}
+
+static inline void process_command_char(char data)
+{
+    // Parse and execute commands if the enter key was hit
+    if (data == '\r') {
+        execute_command(cmd_buffer);
+        return;
+    }
+
+    // Add char to command buffer; cycle on overflow
+    cmd_buffer[cmd_buffer_index] = data;
+    ++cmd_buffer_index;
+    cmd_buffer_index %= CMD_BUFFER_SIZE;
+}
+
+static inline void process_update_data(void)
+{
+    page_buffer[page_buffer_index] = usb_getc();
+    ++page_buffer_index;
+    --update_bytes_pending;
+
+    // TODO: Extract CRC from last page and rewind page_buffer_index
+    // TODO: Pad last page
+    // TODO: When a page is full, write it to the temporary application flash
+    // TODO: Calculate CRC of the written application and compare with expected CRC
+    // TODO: Tell xboot to install the firmware on next reset
+    // TODO: Reset device
+
+    // Return to normal operation if the update failed
+    if (update_bytes_pending == 0) {
+        usb_puts("Update failed!");
+        update_in_progress = false;
+        usb_set_echo(true);
+    }
 }
 
 void serial_communication_task(void)
@@ -198,25 +269,11 @@ void serial_communication_task(void)
         return;
     }
 
-    // Receive character
-    int16_t data = usb_getc();
-
-    // Parse and execute commands
-    switch (data) {
-    case USB_EMPTY_CHAR:
-        // Ignore non-printable (non-ASCII) characters
-        break;
-
-    case '\r':
-        // Parse and execute command
-        execute_command(cmd_buffer);
-        break;
-
-    default:
-        // Add char to command buffer; cycle on overflow
-        cmd_buffer[cmd_buffer_index] = data;
-        ++cmd_buffer_index;
-        cmd_buffer_index %= CMD_BUFFER_SIZE;
-        break;
+    // Just append data to buffer if raw data mode is active
+    if (update_in_progress) {
+        process_update_data();
+    }
+    else {
+        process_command_char(usb_getc());
     }
 }
