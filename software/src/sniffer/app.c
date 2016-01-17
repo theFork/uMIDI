50606eca96d8990cedf78699e3aecc533470cd15
Sebastian Neuser <haggl@sineband.de>
2016-01-17 21:36:20 +0100

    Add MIDI sniffer application

---
+++ software/src/sniffer/app.c
@@ -0,0 +1,63 @@
+/// \file
+/// \brief      Implementation of the MIDI sniffer module
+
+/*
+ * Copyright 2015 Sebastian Neuser
+ *
+ * This file is part of the uMIDI firmware.
+ *
+ * The uMIDI firmware is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The uMIDI firmware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lib/midi.h"
+#include "lib/usb.h"
+
+#include "app.h"
+
+
+////////////////////////////////////////////////////////////////
+//                     V A R I A B L E S                      //
+////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////
+//      F U N C T I O N S   A N D   P R O C E D U R E S       //
+////////////////////////////////////////////////////////////////
+
+bool exec_channel(const char* command)
+{
+    // Abort if the command is malformed
+    if (strlen(command) < 5 || command[3] != ' ') {
+        usb_puts("Malformed command" USB_NEWLINE);
+        return false;
+    }
+
+    enum midi_channel channel = atoi(command+4);
+    if (channel == 0) {
+        usb_puts(PSTR("Listening on all channels"));
+        set_omni_mode(true);
+    }
+    else {
+        usb_printf(PSTR("Listening on MIDI channel %u" USB_NEWLINE), channel);
+        set_midi_rx_channel(channel-1);
+        set_omni_mode(false);
+    }
+
+    return true;
+}
---
+++ software/src/sniffer/config.c
@@ -0,0 +1,76 @@
+/// \file
+/// \brief      Device configuration
+
+/*
+ * Copyright 2015 Sebastian Neuser
+ *
+ * This file is part of the uMIDI firmware.
+ *
+ * the uMIDI firmware is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The uMIDI firmware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the uMIDI firmware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stddef.h>
+
+#include "lib/background_tasks.h"
+#include "lib/leds.h"
+#include "lib/midi.h"
+#include "lib/serial_communication.h"
+#include "lib/usb.h"
+
+#include "config.h"
+#include "app.h"
+
+
+////////////////////////////////////////////////////////////////
+//                     V A R I A B L E S                      //
+////////////////////////////////////////////////////////////////
+
+//---------------- MIDI ----------------//
+struct midi_config midi_config = {
+    .event_handlers = {
+        .control_change = NULL,
+        .note_off       = NULL,
+        .note_on        = NULL,
+        .program_change = NULL
+    },
+    .rx_channel = MIDI_CHANNEL_01,
+    .tx_channel = MIDI_CHANNEL_01,
+    .omni_mode = true
+};
+
+//---------------- Background tasks ----------------//
+background_task_t high_frequency_tasks[] = {
+    &serial_communication_task,
+};
+uint8_t high_frequency_tasks_size = sizeof(high_frequency_tasks)/sizeof(background_task_t);
+
+background_task_t mid_frequency_tasks[] = {
+    &usb_main_task,
+};
+uint8_t mid_frequency_tasks_size = sizeof(mid_frequency_tasks)/sizeof(background_task_t);
+
+background_task_t low_frequency_tasks[] = {
+    &update_leds,
+};
+uint8_t low_frequency_tasks_size = sizeof(low_frequency_tasks)/sizeof(background_task_t);
+
+//---------------- Commands ----------------//
+static const char cmd_string_chn[] PROGMEM = "chn";
+static const char help_string_chn[] PROGMEM = "<n>\n"
+    "Sets the MIDI receive channel to sniff on:\n"
+    "<n> : channel number [1..16] or 'x' for Omni mode";
+struct serial_command serial_commands[] = {
+    { .cmd_string = cmd_string_chn, .help_string = help_string_chn, .handler = &exec_channel },
+};
+uint8_t serial_commands_size = sizeof(serial_commands) / sizeof(struct serial_command);
